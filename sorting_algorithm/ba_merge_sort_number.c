536a3106e3c8d32f19f4373b25db54b2feb6c87b
ricotandrio <ricotandrio@gmail.com>
2023-08-05 10:56:22 +0700

    add floodfill

--- sorting_algorithm/ba_merge_sort_number.c
+++ sorting_algorithm/ba_merge_sort_number.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 
-void conquer(int arr[], int l, int m, int r){
-	int temp[r-l+1], tempIdx = 0;
-	int ml = l, mr = m+1;
+void merge(int arr[], int l, int m, int r){
+	int temp[r-l+1], tempIdx = 0, ml = l, mr = m+1;
+
 	while(ml <= m && mr <= r){
-		if(arr[ml] < arr[mr]){
-			temp[tempIdx++] = arr[ml++];
-		} else {
-			temp[tempIdx++] = arr[mr++];
-		}
+		if(arr[ml] < arr[mr]) temp[tempIdx++] = arr[ml++];
+		else temp[tempIdx++] = arr[mr++];
 	}
-	while(ml <= m){
-		temp[tempIdx++] = arr[ml++];
-	}
+
+	while(ml <= m) temp[tempIdx++] = arr[ml++];
+
 	for(int i = 0; i < tempIdx; i++){
 		arr[i+l] = temp[i];
 	}
@@ -23,7 +20,7 @@ void divide(int arr[], int l, int r){
 		int m = l + (r-l)/2;
 		divide(arr, l, m);
 		divide(arr, m+1, r);
-		conquer(arr, l, m, r);
+		merge(arr, l, m, r);
 	}
 }
 
